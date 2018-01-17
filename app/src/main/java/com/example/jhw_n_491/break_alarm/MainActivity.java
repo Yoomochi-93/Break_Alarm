a8798d393e0be20eaee2fb4487887575cf63b3cf
jh.yu <jh.yu@em-tech.co.kr>
2018-01-17 09:33:20 +0900

    Modify MainAcitivity UI
    1. 메인엑티비티 UI 수정
    2. 알람등록 UI 추가
    
    Change-Id: I6e398e38b77d8dc11536555021120912d8b8cec7

--- app/src/main/java/com/example/jhw_n_491/break_alarm/MainActivity.java
+++ app/src/main/java/com/example/jhw_n_491/break_alarm/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.jhw_n_491.break_alarm;
 
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button registration_btn;
+    ListView alarm_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initUiComponents();
+
+        registration_btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                DialogTimePicker();
+            }
+        });
+    }
+
+    private void DialogTimePicker()
+    {
+        TimePickerDialog.OnTimeSetListener mTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                Toast.makeText(getApplicationContext(), "Time is="+ hourOfDay + ":" + minute,Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        TimePickerDialog alert = new TimePickerDialog(this, mTimeSetListener,0,0,false);
+        alert.show();
+    }
+
+    void initUiComponents()
+    {
+        registration_btn = (Button)findViewById(R.id.registration_btn);
+        alarm_list = (ListView)findViewById(R.id.alarm_list);
     }
 }
