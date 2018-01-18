0bbf9609a3882c10ae8ea1e16380248e51fafbb2
Taehun Kim <taeh.kim@em-tech.co.kr>
2018-01-18 16:59:07 +0900

    [Break_Alarm.Data] Add alarm list to nonvolatile data storage
    1. The SharedPreferences data storage is added
    - The alarm list should be saved even if this app is terminated.
    2. Second parameter is set to be clear Calendar object
    
    Change-Id: I7024c3ba9bee8bf2d45ac7bd4ed1dec19bfacc8a

--- app/src/main/java/com/example/jhw_n_491/break_alarm/MainActivity.java
+++ app/src/main/java/com/example/jhw_n_491/break_alarm/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.jhw_n_491.break_alarm;
 
+import android.app.Activity;
 import android.app.TimePickerDialog;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TimePicker;
 import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,13 +21,17 @@ public class MainActivity extends AppCompatActivity {
     Button registration_btn;
     ListView alarm_list;
     BreakAlarmApplication app;
+    private SharedPreferences sp;
+    private static final String SP_NAME = "Break_Alarm";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         app = (BreakAlarmApplication) getApplicationContext();
+        sp = getSharedPreferences("Break_Alarm", Activity.MODE_PRIVATE);
 
+        getAlarmList();
         initUiComponents();
 
         registration_btn.setOnClickListener(new View.OnClickListener()
@@ -33,6 +43,42 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void getAlarmList() {
+        String str_val = sp.getString("Alarm_List", null);
+        String[] str_val_list = str_val.split("/");
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
+        Log.d("BreakAlarm", "getAlarmList:" + str_val);
+
+        for (String s: str_val_list) {
+            Calendar cal = Calendar.getInstance();
+            Log.d("BreakAlarm", "for:" + s);
+            try {
+                cal.setTime(sdf.parse(s));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            app.setAlarm(cal);
+        }
+    }
+
+    private void updateAlarmList()
+    {
+        SharedPreferences.Editor editor = sp.edit();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        String saved_str = "";
+
+        for (Calendar cal : app.getAlarmList()) {
+            saved_str += sdf.format(cal.getTime());
+            saved_str += "/";
+            Log.d("BreakAlarm", "puAlarmList for: " + saved_str);
+        }
+
+        editor.putString("Alarm_List", saved_str);
+        Log.d("BreakAlarm", "puAlarmList: " + saved_str);
+        editor.apply();
+    }
+
     private void DialogTimePicker()
     {
         TimePickerDialog.OnTimeSetListener mTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
@@ -42,7 +88,10 @@ public class MainActivity extends AppCompatActivity {
                 Calendar cal = Calendar.getInstance();
                 cal.add(Calendar.HOUR, hourOfDay);
                 cal.add(Calendar.MINUTE, minute);
+                cal.add(Calendar.SECOND, 0);
+
                 app.setAlarm(cal);
+                updateAlarmList();
             }
         };
 
